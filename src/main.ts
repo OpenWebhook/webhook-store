2703e3c716c8a98983b1063ae7a3297e116d6d8c
Sammy Teillet <sammy.teillet@gmail.com>
2022-04-05 08:35:40 +0200

    chore(): Enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
   await app.listen(process.env.PORT || 3001);
