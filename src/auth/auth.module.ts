5810786535567426acc197d605b6247a40497501
Sammy Teillet <sammy.teillet@gmail.com>
2022-04-27 13:15:01 +0200

    wip

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [
-    ConfigModule,
-    PassportModule,
-    JwtModule.register({
-      signOptions: { expiresIn: '60s' },
-    }),
-  ],
+  imports: [ConfigModule],
   providers: [AuthService, JwtStrategy],
   exports: [AuthService],
 })
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,17 +1,18 @@
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ExtractJwt, Strategy, StrategyOptions } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private configService: ConfigService) {
+  constructor(configService: ConfigService) {
     const secret = configService.get<string>('auth.jwtPublicKey');
-    super({
+    const strategyOption: StrategyOptions = {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: secret,
-    });
+    };
+    super(strategyOption);
   }
 
   async validate(payload: any) {
