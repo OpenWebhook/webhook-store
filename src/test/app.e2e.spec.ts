9d3860223e30f77b2b6cfdd50f2c14b2602a60d6
Sammy Teillet <sammy.teillet@gmail.com>
2022-05-03 08:00:42 +0200

    feature(): serve static files

--- src/test/app.e2e.spec.ts
+++ src/test/app.e2e.spec.ts
@@ -26,7 +26,7 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/hello')
       .expect(200)
       .expect('0 Hello World!');
   });
