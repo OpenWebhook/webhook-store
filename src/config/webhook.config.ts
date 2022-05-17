a974e141b8e26165d10cabe6ea74283fca422f85
Samox <sammy.teillet@gmail.com>
2022-05-17 19:40:13 +0200

    refactor(): Remove useless assertion since type already infered

--- src/config/webhook.config.ts
+++ src/config/webhook.config.ts
@@ -1,5 +1,5 @@
 export default () => ({
-  maxStoredWebhookPerHost: (process.env.MAX_STORED_WEBHOOKS_PER_HOST
+  maxStoredWebhookPerHost: process.env.MAX_STORED_WEBHOOKS_PER_HOST
     ? parseInt(process.env.MAX_STORED_WEBHOOKS_PER_HOST, 10)
-    : null) as number | null,
+    : null,
 });
