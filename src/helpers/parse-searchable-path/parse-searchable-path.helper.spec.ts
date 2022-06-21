786f362c71f2bf3917a81affb47c36a6b4da2d0e
Samox <sammy.teillet@gmail.com>
2022-06-21 16:17:36 +0200

    feature(Webhook): enable search in path

---
+++ src/helpers/parse-searchable-path/parse-searchable-path.helper.spec.ts
@@ -0,0 +1,11 @@
+import { pathToPSQLTsQuery } from './parse-searchable-path.helper';
+
+describe('pathToPSQLTsQuery', () => {
+  it('Should return undefined if given undefined', () => {
+    expect(pathToPSQLTsQuery(undefined)).toBeUndefined();
+  });
+
+  it('Should return - / - except for the first character', () => {
+    expect(pathToPSQLTsQuery('/croute/lol')).toBe('croute - lol');
+  });
+});
