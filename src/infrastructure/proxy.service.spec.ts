c0abcb3a12f463c5432128c81b800b4521442422
Samox <sammy.teillet@gmail.com>
2022-07-09 14:12:57 +0200

    test(Proxy): Remove duplication

--- src/infrastructure/proxy.service.spec.ts
+++ src/infrastructure/proxy.service.spec.ts
@@ -4,7 +4,7 @@ import { ProxyService } from './proxy.service';
 import { of, throwError } from 'rxjs';
 import { right, left } from 'fp-ts/lib/Either';
 
-describe('proxy.servce.ts', () => {
+describe('proxy.service.ts', () => {
   let proxyService: ProxyService;
   let httpService: HttpService;
 
@@ -13,42 +13,41 @@ describe('proxy.servce.ts', () => {
     proxyService = new ProxyService(httpService);
   });
 
-  it('should call postmethod', async () => {
-    const result: AxiosResponse = {
-      data: {},
-      status: 200,
-      statusText: 'OK',
-      headers: {},
-      config: {},
+  describe('Happy path', () => {
+    it('should call postmethod', async () => {
+      const result: AxiosResponse = {
+        data: {},
+        status: 200,
+        statusText: 'OK',
+        headers: {},
+        config: {},
+      };
+      jest.spyOn(httpService, 'post').mockImplementationOnce(() => of(result));
+      expect(proxyService).toBeDefined();
+      const res = await proxyService.sendWebhook('', {}, {}, '');
+      expect(httpService.post).toHaveBeenCalled();
+      expect(res).toEqual(right('OK'));
+    });
+  });
+
+  describe('Error path', () => {
+    const mockPostToTrhowError = (err: any) => {
+      jest
+        .spyOn(httpService, 'post')
+        .mockImplementationOnce(() => throwError(() => err));
     };
-    jest.spyOn(httpService, 'post').mockImplementationOnce(() => of(result));
-    expect(proxyService).toBeDefined();
-    const res = await proxyService.sendWebhook('', {}, {}, '');
-    expect(httpService.post).toHaveBeenCalled();
-    expect(res).toEqual(right('OK'));
-  });
 
-  it('should not throw if post fails with axios error', async () => {
-    jest
-      .spyOn(httpService, 'post')
-      .mockImplementationOnce(() =>
-        throwError(() => new AxiosError('Page not found')),
-      );
-    expect(proxyService).toBeDefined();
-    const res = await proxyService.sendWebhook('', {}, {}, '');
-    expect(httpService.post).toHaveBeenCalled();
-    expect(res).toEqual(left('err'));
-  });
+    it('should not throw if post fails with axios error', async () => {
+      mockPostToTrhowError(new AxiosError('Page not found'));
+      const res = await proxyService.sendWebhook('unkownUrl', {}, {}, '');
+      expect(httpService.post).toHaveBeenCalled();
+      expect(res).toEqual(left('err'));
+    });
 
-  it('should not throw if post fails with random error', async () => {
-    jest
-      .spyOn(httpService, 'post')
-      .mockImplementationOnce(() =>
-        throwError(() => new Error("Network error can't connect to server")),
-      );
-    expect(proxyService).toBeDefined();
-    const res = await proxyService.sendWebhook('', {}, {}, '');
-    expect(httpService.post).toHaveBeenCalled();
-    expect(res).toEqual(left('err'));
+    it('should not throw if post fails with random error', async () => {
+      mockPostToTrhowError(new Error("Network error can't connect to server"));
+      const res = await proxyService.sendWebhook('invalidurl', {}, {}, '');
+      expect(res).toEqual(left('err'));
+    });
   });
 });
