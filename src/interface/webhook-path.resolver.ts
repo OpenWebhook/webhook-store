7a9aa250fd4e79dcfbfc16d26e130e45b8d0ad0d
Samox <sammy.teillet@gmail.com>
2022-07-12 11:15:20 +0200

    feature(WebhookPAths): Get all distinct paths

--- src/interface/webhook-path.resolver.ts
+++ src/interface/webhook-path.resolver.ts
@@ -1,11 +1,18 @@
 import { Query, Resolver } from '@nestjs/graphql';
+import { AppService } from '../application/webhook/webhook.service';
+import { Hostname } from './decorators/hostname.decorator';
 import { WebhookPathModel } from './webhook-path.model';
 import { WebhookModel } from './webhook.model';
 
 @Resolver(() => WebhookModel)
 export class WebhookPathResolver {
+  constructor(private readonly appService: AppService) {}
+
   @Query(() => [WebhookPathModel])
-  async webhookPaths(): Promise<WebhookPathModel[]> {
-    return [];
+  async webhookPaths(
+    @Hostname() hostname: string,
+  ): Promise<WebhookPathModel[]> {
+    const webhookPaths = await this.appService.getWebhooksPath(hostname);
+    return webhookPaths;
   }
 }
