e4177278e539bc9ba74b56ef6e34789208780378
Samox <sammy.teillet@gmail.com>
2022-07-27 10:36:02 +0200

    feature(Webhook): Handle xml

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './infrastructure/prisma.service';
+import * as xmlparser from 'express-xml-bodyparser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(xmlparser());
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
   await app.listen(process.env.PORT || 3000);
