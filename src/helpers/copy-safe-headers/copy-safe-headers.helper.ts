83559e43c105f1a43be9f39e585da2e4c7fb18a9
Samox <sammy.teillet@gmail.com>
2022-07-27 10:49:06 +0200

    feature(Proxy): Remove content type from header so Axios sets it itself

--- src/helpers/copy-safe-headers/copy-safe-headers.helper.ts
+++ src/helpers/copy-safe-headers/copy-safe-headers.helper.ts
@@ -2,7 +2,7 @@ export const copySafeHeaders = (
   headers: Readonly<Record<string, string>>,
 ): Record<string, string> => {
   const UNSAFE_HEADERS_REGEXP =
-    /^(?:host|origin|cookie|user-agent|content-length|version|cdn-loop|cf-ray|x-scheme|x-real-ip|cf-ipcountry|x-request-id|x-forwarded-for|cf-connecting-ip|x-forwarded-host|x-forwarded-port|x-forwarded-proto|x-forwarded-scheme|x-original-forwarded-for)$/i;
+    /^(?:host|origin|cookie|user-agent|content-length|version|cdn-loop|cf-ray|x-scheme|x-real-ip|cf-ipcountry|x-request-id|x-forwarded-for|cf-connecting-ip|x-forwarded-host|x-forwarded-port|x-forwarded-proto|x-forwarded-scheme|x-original-forwarded-for|content-type)$/i;
   const safeHeaders = Object.entries(headers).reduce((acc, [key, value]) => {
     if (!key.match(UNSAFE_HEADERS_REGEXP)) {
       acc[key] = value;
