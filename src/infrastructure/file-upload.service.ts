6c280dfcd43d320a15843dfd8cb30d2c291e8b41
Samox <sammy.teillet@gmail.com>
2022-07-28 16:30:19 +0200

    refactor(Storage): Move storgae function in class

--- src/infrastructure/file-upload.service.ts
+++ src/infrastructure/file-upload.service.ts
@@ -7,36 +7,34 @@ const s3 = new S3({
   endpoint: process.env.S3_BUCKET_ENDPOINT,
 });
 
-export const uploadRequestFile = async (file: Express.Multer.File) => {
-  try {
-    const unixTime = Math.floor(+new Date() / 1000);
-    const params = {
-      Bucket: 'webhook-store',
-      Key: `${process.env.S3_BUCKET_PREFIX_PATH}${unixTime}_${file.originalname}`, // file name with timestamp
-      Body: file.buffer,
-      ACL: 'public-read', // public read policy
-      ContentType: file.mimetype,
-    };
-
-    // Uploading files to the bucket
+@Injectable()
+export class FileUploadService {
+  public async uploadRequestFile(file: Express.Multer.File): Promise<any> {
     try {
-      const result = await new Promise((resolve, reject) => {
-        s3.upload(params, (err?: any, data?: S3.ManagedUpload.SendData) => {
-          err && reject(err);
-          data && resolve(data);
-        });
-      });
+      const unixTime = Math.floor(+new Date() / 1000);
+      const params = {
+        Bucket: 'webhook-store',
+        Key: `${process.env.S3_BUCKET_PREFIX_PATH}${unixTime}_${file.originalname}`, // file name with timestamp
+        Body: file.buffer,
+        ACL: 'public-read', // public read policy
+        ContentType: file.mimetype,
+      };
 
-      console.log(result);
+      // Uploading files to the bucket
+      try {
+        const result = await new Promise((resolve, reject) => {
+          s3.upload(params, (err?: any, data?: S3.ManagedUpload.SendData) => {
+            err && reject(err);
+            data && resolve(data);
+          });
+        });
+
+        console.log(result);
+      } catch (err) {
+        console.error(err);
+      }
     } catch (err) {
       console.error(err);
     }
-  } catch (err) {
-    console.error(err);
   }
-};
-
-@Injectable()
-export class FileUploadService {
-  public uploadRequestFile = uploadRequestFile;
 }
