4e5361c9d17335e90e114f64e75b2df6b7bef115
Samox <sammy.teillet@gmail.com>
2022-08-01 16:19:59 +0200

    feature(Response): Delete response when webhook is deleted

---
+++ prisma/migrations/20220801141711_delete_on_cascade_webhook_response/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "Response" DROP CONSTRAINT "Response_webhookId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Response" ADD CONSTRAINT "Response_webhookId_fkey" FOREIGN KEY ("webhookId") REFERENCES "Webhook"("id") ON DELETE CASCADE ON UPDATE CASCADE;
