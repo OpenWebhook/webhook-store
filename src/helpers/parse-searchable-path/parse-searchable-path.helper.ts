707dec5ffe2258910821c1ff1cf8d118d3317ca0
Sammy Teillet <sammy.teillet@gmail.com>
2022-08-04 09:58:26 +0400

    Refactor functionnal programming (#62)
    * refactor(Webhook): body factory returns a task
    
    * refactor(Webhook): addWebhook returns a task
    
    * refactor(Webhook): handleWebhooks pipes tasks
    
    * refactor(Uuid): use type io for randomly generated id
    
    * refactor(Webhook): transform function to Task
    
    * refactor(Webhook): Use function paradigm in getWebhooks
    
    * refactor(Webhook): Use function more paradigm in getWebhooks
    
    * refactor(Webhook): Extract function in private method
    
    * refactor(Webhook): remove useless function wrapping
    
    * refactor(Webhook): apply functionnal paradigm to delete method
    
    * refactor(Webhook): apply fp paradigm to path helper
    
    * bugfix(Webhook): Properly parse hostname

--- src/helpers/parse-searchable-path/parse-searchable-path.helper.ts
+++ src/helpers/parse-searchable-path/parse-searchable-path.helper.ts
@@ -1,22 +1,32 @@
+import { pipe } from 'fp-ts/function';
+
 function removeTrailingSlash(str: string): string {
   return str.replace(/\/+$/, '');
 }
 
-export const pathToSearchablePath = (path: string): string => {
-  const pathWithATrailingSlash =
-    path[path.length - 1] === '/' ? path : `${path}/`;
-  const pathWithoutUUIDs = pathWithATrailingSlash.replace(
+function removeUuids(str: string): string {
+  return str.replace(
     /\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\//g,
     '/:id/',
   );
+}
 
-  const pathWithoutNumeralIds = pathWithoutUUIDs.replace(
-    /\/[0-9]*\//g,
-    '/:id/',
+function removeNumeralIds(str: string): string {
+  return str.replace(/\/[0-9]*\//g, '/:id/');
+}
+
+function addTrailingSlash(str: string): string {
+  return str[str.length - 1] === '/' ? str : `${str}/`;
+}
+
+export const pathToSearchablePath = (path: string): string => {
+  return pipe(
+    path,
+    addTrailingSlash,
+    removeUuids,
+    removeNumeralIds,
+    removeTrailingSlash,
   );
-
-  const pathWithoutTrailingSlash = removeTrailingSlash(pathWithoutNumeralIds);
-  return pathWithoutTrailingSlash;
 };
 
 export const pathIsValid = (path: string | undefined): path is `/${string}` => {
