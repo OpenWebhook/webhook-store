79cac4dc3b3aad823f38bd88d1c5740f11893538
Sammy Teillet <sammy.teillet@gmail.com>
2022-08-04 16:54:08 +0400

    chore(CD): Add another load test for pushing webhooks (#64)
    * chore(CD): Add another load test for pushing webhooks
    
    * chore(CD): Make load testing on demo env
    
    * chore(CI): Add thresholds for laod tests

--- load-test/get-100-webhooks.js
+++ load-test/get-100-webhooks.js
@@ -1,5 +1,5 @@
 import http from 'k6/http';
-import { check } from 'k6';
+import { check, sleep } from 'k6';
 
 const query = `query {
   webhooks(first: 100) {
@@ -10,13 +10,21 @@ const query = `query {
 
 export default function () {
   const res = http.post(
-    'https://webhook-store.herokuapp.com/graphql',
+    'https://k6.dev.webhook.store/graphql',
     JSON.stringify({ query: query }),
     {
       headers: { 'Content-Type': 'application/json' },
     },
   );
+  check(res.status, 200);
   const body = JSON.parse(res.body);
-
   check(body.data.webhooks.length, 100);
+  sleep(0.2);
 }
+
+export const options = {
+  thresholds: {
+    // fail the test if 95th percentile response goes above 500ms
+    http_req_duration: ['p(95)<1000'],
+  },
+};
