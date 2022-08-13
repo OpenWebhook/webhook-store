efd29e5fa52b911d51bcf4151f377481e351266d
Samox <sammy.teillet@gmail.com>
2022-08-13 11:14:30 +0200

    refactor(Path): Use flow instead of pipe

--- src/helpers/parse-searchable-path/parse-searchable-path.helper.ts
+++ src/helpers/parse-searchable-path/parse-searchable-path.helper.ts
@@ -1,4 +1,4 @@
-import { pipe } from 'fp-ts/function';
+import { flow } from 'fp-ts/function';
 import { string, array } from 'fp-ts';
 
 const uuidRegex =
@@ -20,17 +20,14 @@ const stringIsNotEmpty = (s: string): boolean => {
   return !string.isEmpty(s);
 };
 
-export const pathToSearchablePath = (path: string): string => {
-  return pipe(
-    path,
-    parsePathOnSlash,
-    array.filter(stringIsNotEmpty),
-    array.map(string.replace(uuidRegex, ':id')),
-    array.map(string.replace(numeralIdRegex, ':id')),
-    array.map(string.replace(randomCharIdRegex, ':id')),
-    joinPathWithSlash,
-  );
-};
+export const pathToSearchablePath: (path: string) => string = flow(
+  parsePathOnSlash,
+  array.filter(stringIsNotEmpty),
+  array.map(string.replace(uuidRegex, ':id')),
+  array.map(string.replace(numeralIdRegex, ':id')),
+  array.map(string.replace(randomCharIdRegex, ':id')),
+  joinPathWithSlash,
+);
 
 export const pathIsValid = (path: string | undefined): path is `/${string}` => {
   if (path) {
