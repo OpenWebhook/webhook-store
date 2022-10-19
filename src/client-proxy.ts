e686c87a798c07956b004bd78bf8f38e240897ec
Samox <sammy.teillet@gmail.com>
2022-10-19 15:37:36 +0200

    feature(): Follow redirect in proxy so client is not redirected

--- src/client-proxy.ts
+++ src/client-proxy.ts
@@ -1,7 +1,7 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const CLIENT_URL =
-  process.env.CLIENT_URL || 'https://demo.openwebhook.io/releases/v1/';
+  process.env.CLIENT_URL || 'https://demo.openwebhook.io/releases/v1';
 
 const pathFilter = function (path: string, req: any) {
   return req.method === 'GET' && path !== '/graphql' && path !== '/playground';
@@ -10,6 +10,7 @@ const pathFilter = function (path: string, req: any) {
 export const clientProxy = createProxyMiddleware(pathFilter, {
   target: CLIENT_URL,
   changeOrigin: true,
+  followRedirects: true,
   pathRewrite: {
     '^(.(?!.*.css$|.*.js.map$|.*.js$|.*.html))*$': '',
   },
