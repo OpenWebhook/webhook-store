15f0722a964e63c95e8b5ec8470fa45aefbf0ba7
Sammy Teillet <sammy.teillet@gmail.com>
2023-03-15 15:23:57 +0100

    test: add failing test for unauthenticated GQL request on protected endpoint

---
+++ src/test/protected-app.e2e.spec.ts
@@ -0,0 +1,60 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request = require('supertest');
+import { AppModule } from '../app.module';
+import { PrismaService } from '../infrastructure/prisma.service';
+import { WsAdapter } from '@nestjs/platform-ws';
+
+jest.mock('../helpers/get-hostname/get-hostname.helper');
+import { getHostnameOrLocalhost } from '../helpers/get-hostname/get-hostname.helper';
+jest.mock('../config/auth.config');
+import authConfig from '../config/auth.config';
+
+const hostname = 'webhook.resolver.e2e.spec';
+(getHostnameOrLocalhost as jest.Mock).mockImplementation(() => hostname);
+
+(authConfig as unknown as jest.Mock).mockReturnValue({
+  jwksUri:
+    process.env.JWKS_URI ||
+    `https://openwebhook-auth.herokuapp.com/.well-known/jwks.json`,
+  isProtected: true,
+  adminPassword: 'adminPassword',
+});
+
+describe('CustomerResolver (e2e)', () => {
+  let app: INestApplication;
+  let prismaService: PrismaService;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    prismaService = app.get(PrismaService);
+    app.useWebSocketAdapter(new WsAdapter(app));
+    await app.init();
+    await prismaService.webhook.deleteMany({ where: { host: hostname } });
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  const gql = '/graphql';
+
+  describe('getWebhooks', () => {
+    it.only('should get an error if un authenticated', () => {
+      return request(app.getHttpServer())
+        .post(gql)
+        .send({
+          query: 'query {webhooks {id}}',
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(Array.isArray(res.body.data)).toBeFalsy();
+          expect(res.body.errors[0].message).toBe('Unauthorized');
+        });
+    });
+  });
+});
